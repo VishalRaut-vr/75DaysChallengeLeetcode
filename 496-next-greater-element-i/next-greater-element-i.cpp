ee3220c20067c7013068e340bf4dcb9d7014ba7a
VishalRaut-vr <rautv6491@gmail.com>
2026-08-17 01:54:30 +0530

    Time: 1 ms (53.91%) | Memory: 12.8 MB (63.39%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -5,15 +5,16 @@ public:
        stack<int> s;
 
        for(int i=nums2.size()-1; i>=0; i--){
-        while(s.size() > 0 && s.top() <= nums2[i]){
+        while(s.size() > 0 && s.top() < nums2[i]){
             s.pop();
-    
         }
+
         if(s.empty()){
             m[nums2[i]] = -1;
         }else{
             m[nums2[i]] = s.top();
         }
+
         s.push(nums2[i]);
        }
 
@@ -22,5 +23,5 @@ public:
         ans.push_back(m[nums1[i]]);
        }
        return ans;
-    }
+    }  
 };
