ee318f748699182d9cd6b45625077418cfa7007c
VishalRaut-vr <rautv6491@gmail.com>
2026-08-19 19:33:44 +0530

    Time: 19 ms (74.65%) | Memory: 139.3 MB (26.36%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,31 +4,26 @@ public:
         deque<int> dq;
         vector<int> res;
 
-        //first window
-        for(int i=0; i<k; i++){
-            while(dq.size() > 0 && nums[dq.back()] <= nums[i]){
+        for (int i = 0; i < k; i++) {
+            while (dq.size() > 0 && nums[dq.back()] <= nums[i]) {
                 dq.pop_back();
             }
             dq.push_back(i);
         }
 
-        for(int i=k; i<nums.size(); i++){
+        for (int i = k; i < nums.size(); i++) {
             res.push_back(nums[dq.front()]);
 
-            while(dq.size() > 0 && dq.front() <= i-k){
+            // remove element which is not part of the curr window
+            while (dq.size() > 0 && dq.front() <= i - k) {
                 dq.pop_front();
             }
 
-            //romove the smaller value
-
-            while(dq.size() > 0 && nums[dq.back()] <= nums[i]){
+            // remove elements which are smaller
+            while (dq.size() > 0 && nums[dq.back()] <= nums[i]) {
                 dq.pop_back();
             }
-
             dq.push_back(i);
-
-            
-            
         }
         res.push_back(nums[dq.front()]);
 
