27af9af3078ebeba497693897b3b2773500a18bc
VishalRaut-vr <rautv6491@gmail.com>
2026-08-22 01:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (63.84%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp(n,0);
+        for(int i=0; i<n; i++){
+            temp[i] = nums[nums[i]];
+        }
+        return temp;
+    }
+};
